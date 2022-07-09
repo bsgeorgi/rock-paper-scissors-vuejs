21da9efbedad791669a7c076e3b7cc9f4e161847
Georgi Botsihhin <botsihhingeorge@icloud.com>
2022-07-09 10:53:51 +0100

    feat: move score board into its own component

---
+++ src/components/ScoreBoard.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <div class="border-4 border-white py-3 px-16 font-bold text-5xl text-white">
+            <span>{{ firstPlayerScore ?? 0 }}</span>
+            <span>:</span>
+            <span>{{ secondPlayerScore ?? 0 }}</span>
+        </div>
+        <div class="absolute -mt-16 -ml-20 font-bold backdrop-blur-md bg-white/30 px-4 py-2 w-28 text-white">
+            <span>{{ props.mode == 'pvc' ? 'Player' : 'Computer' }}</span>
+        </div>
+        <div class="absolute -mt-16 ml-44 font-bold backdrop-blur-md bg-white/30 px-4 py-2 w-28 text-white">
+            <span>Computer</span>
+        </div>
+    </div>
+</template>
+
+<script setup>
+const props = defineProps({
+    mode: String,
+    firstPlayerScore: Number,
+    secondPlayerScore: Number
+});
+</script>
--- src/views/PlayerVsComputer.vue
+++ src/views/PlayerVsComputer.vue
@@ -1,18 +1,6 @@
 <template>
     <div class="flex flex-col items-center justify-center space-y-16">
-        <div>
-            <div class="border-4 border-white py-3 px-16 font-bold text-5xl text-white">
-                <span>0</span>
-                <span>:</span>
-                <span>0</span>
-            </div>
-            <div class="absolute -mt-16 -ml-20 font-bold backdrop-blur-md bg-white/30 px-4 py-2 w-28 text-white">
-                <span>Player</span>
-            </div>
-            <div class="absolute -mt-16 ml-44 font-bold backdrop-blur-md bg-white/30 px-4 py-2 w-28 text-white">
-                <span>Computer</span>
-            </div>
-        </div>
+        <score-board mode="pvc" :secondPlayerScore="3"></score-board>
 
         <div>
             <h5 class="text-white text-3xl font-bold">Something goes here! Whoop whoop!</h5>
@@ -30,4 +18,5 @@
 
 <script setup>
 import SelectionButton from '../components/SelectionButton.vue';
+import ScoreBoard from '../components/ScoreBoard.vue';
 </script>
